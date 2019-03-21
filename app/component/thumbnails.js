be541809b2314367e883b8d2a7f24aaf1d5bb284
Pierre Bretéché <pbreteche@dawan.fr>
2019-03-21 17:06:29 +0100

    affichage de la liste de miniatures

--- app/component/thumbnails.js
+++ app/component/thumbnails.js
@@ -1,7 +1,14 @@
 import {Component} from "./component.js";
+import {IMAGE_LIST} from "../image-list.js";
 
 export class Thumbnails extends Component {
     init() {
-        this.root.innerHTML = 'Miniatures';
+        this.root.innerHTML = Thumbnails.displayList(IMAGE_LIST);
+    }
+
+    static displayList(list) {
+        const htmlImgArray = list.map(img => `<li><img src="img/${img}" /></li>`);
+        const htmlImgString = htmlImgArray.join('');
+        return `<ul>${htmlImgString}</ul>`;
     }
 }
---
+++ app/image-list.js
@@ -0,0 +1,21 @@
+export const IMAGE_LIST = [
+  'acatos.png',
+  'albator.png',
+  'barracuda.png',
+  'dark-knight-joker.png',
+  'freddy-krueger.png',
+  'harry-pottux.png',
+  'kenny.png',
+  'mario.png',
+  'matrix.png',
+  'm.png',
+  'napolétux.png',
+  'narutux.png',
+  'pilot.png',
+  'tortux-ninja.png',
+  'tux34.png',
+  'tux-croft.png',
+  'tux-nroses.png',
+  'tux-slipknot.png',
+  'tuxwalker.png'
+];
